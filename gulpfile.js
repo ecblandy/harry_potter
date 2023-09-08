9a20caa4afcb51efeb636a3b99f236f1d4c479cc
Vinícius Blandy <viny19381@hotmail.com>
2023-09-08 12:13:28 -0300

    corrigindo gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp')
 const imagemin = require('gulp-imagemin')
 const sass = require('gulp-sass')(require('sass'))
-const uglify = require('gulp-uglify')
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -15,11 +14,6 @@ function images() {
     .pipe(gulp.dest('./dist/conteudo'))
 }
 
-function scripts(){
-    return gulp.src('./src/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/scripts'))
-}
 
 exports.default = gulp.parallel(styles, images)
 exports.watch = () => {
